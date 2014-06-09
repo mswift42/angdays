3cc2e9ba7dd78b62d259b32f49adda338277e4eb
martin.haesler <martin.haesler@gmail.com>
2014-06-09 20:19:31 +0100

    Add tasklistkey fn

--- angdays.go
+++ angdays.go
@@ -3,14 +3,19 @@ package angdays
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"appengine"
+	"appengine/datastore"
 )
 
 type Task struct {
-	Id        int64
-	User      string
-	Content   string
-	Scheduled string
-	Done      bool
+	Id        int64     `json:"id" datastore:"-"`
+	User      string    `json:"user"`
+	Content   string    `json:"content" datastore:",noindex"`
+	Scheduled string    `json:"scheduled"`
+	Done      bool      `json:"done"`
+	Created   time.Time `json:"created"`
 }
 
 func init() {
@@ -19,4 +24,24 @@ func init() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, world!")
+}
+func tasklistkey(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "Task", "default_tasklist", 0, nil)
+
+}
+func (t *Task) key(c appengine.Context) *datastore.Key {
+	if t.Id == 0 {
+		t.Created = time.Now()
+		return datastore.NewIncompleteKey(c, "Task", tasklistkey(c))
+	}
+	return datastore.NewKey(c, "Task", "", t.Id, tasklistkey(c))
+}
+
+func (t *Task) save(c appengine.Context) (*Task, error) {
+	k, err := datastore.Put(c, t.key(c), t)
+	if err != nil {
+		return nil, err
+	}
+	t.Id = k.IntID()
+	return t, nil
 }
