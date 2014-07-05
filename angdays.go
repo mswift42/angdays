1940079bdce7e329fa5669fa6aa5a2cffd1646e4
martin.haesler <martin.haesler@gmail.com>
2014-07-05 07:27:14 +0100

    Add agendaoverview struct and fns

--- angdays.go
+++ angdays.go
@@ -21,6 +21,80 @@ type Task struct {
 	Created   time.Time `json:"created"`
 }
 
+// Agenda - struct for Overview of upcoming tasks.
+// Contains a date in format <Weekday, day, monthabbr year>
+// and a slice of tasks for the date.
+type Agenda struct {
+	FancyDate string
+	Taskslice []Task
+}
+
+// agendasize constant, describes size of agendaoverview in days
+const (
+	agendasize int64 = 10
+)
+
+// parseTime - convert a time string with layout
+// dd/mm/yyyy to time.Time type.
+func parseTime(s string) time.Time {
+	layout := "02/01/2006"
+	t, _ := time.Parse(layout, s)
+	return t
+}
+
+// formatDate - convert a time.Time type
+// to a string with layout dd/mm/yyyy
+func formatDate(t time.Time) string {
+	layout := "02/01/2006"
+	return t.Format(layout)
+}
+
+func formatDateFancy(t time.Time) string {
+	layout := "Monday, 02 Jan 2006"
+	return t.Format(layout)
+}
+
+// weekDates - takes a day
+// and returns a slice of dates of range startday - 10 days from startday.
+func weekDates(day time.Time) []time.Time {
+	week := make([]time.Time, agendasize)
+	for i := int64(0); i < agendasize; i++ {
+		week[i] = addDay(day, i-3)
+	}
+
+	return week
+}
+
+// addDay - add to a given starting day, a number of days
+// and return the resulting date.
+func addDay(startday time.Time, day int64) time.Time {
+	length := 24 * day
+	return startday.Add(time.Duration(length) * time.Hour)
+}
+
+// agendaOverview - takes a taskslice and a day
+// and builds an overview of all coming dates in range of today -
+// agendasize. For every day it builds a struct agenda with a
+// formatted date and a slice of tasks, due at that date and with
+// status "Todo". Finally the slice of 'Agendastructs is returned.
+func agendaOverview(ts []Task, d time.Time) []Agenda {
+	week := weekDates(d)
+	a := make([]Agenda, agendasize)
+	for i, j := range week {
+		a[i].FancyDate = formatDateFancy(j)
+	}
+	for i := range a {
+		ag := make([]Task, 0)
+		for _, k := range ts {
+			if formatDate(week[i]) == k.Scheduled && k.Done == "Todo" {
+				ag = append(ag, k)
+			}
+		}
+		a[i].Taskslice = ag
+	}
+	return a
+}
+
 func tasklistkey(c appengine.Context) *datastore.Key {
 	return datastore.NewKey(c, "Task", "default_tasklist", 0, nil)
 
