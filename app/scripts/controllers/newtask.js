e74e4bf3d68672963c1c59070ef99fe55f27da58
martin.haesler <martin.haesler@gmail.com>
2015-02-28 13:56:29 +0000

    hide taskcontent until task summary has focus

--- app/scripts/controllers/newtask.js
+++ app/scripts/controllers/newtask.js
@@ -8,10 +8,11 @@
  * Controller of the angDaysApp
  */
 angular.module('angDaysApp')
-  .controller('NewtaskCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    .controller('NewtaskCtrl', function ($scope) {
+        $scope.revealContent = function () {
+            console.log($scope.showContent);
+            $scope.showContent = !$scope.showContent;
+        };
+
+        
   });
