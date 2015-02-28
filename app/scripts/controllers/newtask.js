e763432c649ffed5ae45e8864b2edda25418857f
martin.haesler <martin.haesler@gmail.com>
2015-02-28 12:56:57 +0000

    add newtask ctrl

---
+++ app/scripts/controllers/newtask.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angDaysApp.controller:NewtaskCtrl
+ * @description
+ * # NewtaskCtrl
+ * Controller of the angDaysApp
+ */
+angular.module('angDaysApp')
+  .controller('NewtaskCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
