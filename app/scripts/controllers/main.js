05d0547db43dbd6a93d3e03dc946794b70514f47
martin.haesler <martin.haesler@gmail.com>
2015-03-06 19:33:59 +0000

    add dblclickdirective

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,5 @@
  */
 angular.module('angDaysApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+      $scope.tasks = [];
   });
--- app/scripts/directives/dblclickdirective.js
+++ app/scripts/directives/dblclickdirective.js
@@ -7,12 +7,12 @@
  * # dblClickDirective
  */
 angular.module('angDaysApp')
-  .directive('dblClickDirective', function () {
-    return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the dblClickDirective directive');
-      }
-    };
-  });
+    .directive('dblClickDirective', function () {
+        return {
+            scope: {},
+            restrict: 'A',
+            link: function postLink(scope, element, attrs) {
+                element.text('this is the dblClickDirective directive');
+            }
+        };
+    });
