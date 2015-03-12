c638f969a85c6a20932aee241283aea843c43665
martin.haesler <martin.haesler@gmail.com>
2015-03-12 20:10:18 +0000

    add formatDate and nextWeek fn

--- app/scripts/services/dateservice.js
+++ app/scripts/services/dateservice.js
@@ -10,14 +10,22 @@
 angular.module('angDaysApp')
   .factory('dateService', function () {
     // Service logic
-    // ...
+      // ...
 
-    var meaningOfLife = 42;
+      var datefactory = {};
+      
+      datefactory.formatDate = function(date) {
+          return date.toLocaleString().split(' ')[0];
+      };
+
+      datefactory.nextWeek = function(day) {
+          var dayms = day.getTime();
+          return datefactory.formatDate(dayms + (7 * 24 * 60 * 60 * 1000));
+      };
+
+      return datefactory;
+          
+          
 
     // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
-      }
-    };
   });
