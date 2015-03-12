86d301162b9d927e897d165035a682ab91ce1f53
martin.haesler <martin.haesler@gmail.com>
2015-03-12 21:26:28 +0000

    if no date is specified schedule task one week from now

--- app/scripts/controllers/newtask.js
+++ app/scripts/controllers/newtask.js
@@ -8,15 +8,21 @@
  * Controller of the angDaysApp
  */
 angular.module('angDaysApp')
-    .controller('NewtaskCtrl', function ($scope,$http,shareTasks) {
+    .controller('NewtaskCtrl', function ($scope,$http,shareTasks, dateService) {
         $scope.hideContent = true;
         $scope.revealContent = function () {
             $scope.hideContent = !$scope.hideContent;
         };
         $scope.saveTask = function() {
+            
             var task = {"summary":$scope.formData.summary,
-                        "content": $scope.formData.content,
-                        "scheduled": $scope.formData.scheduled};
+                        "content": $scope.formData.content};
+            var sched = $scope.formData.scheduled;
+            if (sched !== undefined) {
+                task.scheduled = new Date(sched);
+            } else {
+                task.scheduled = dateService.nextWeek(new Date());
+            }
             $http.post("/tasks",task).success(function() {
                                      $scope.hideContent = true;
                 $scope.formData.summary = '';
