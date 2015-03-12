d7dcaebc1595e5a371c37638e05db4d8f21b93ba
martin.haesler <martin.haesler@gmail.com>
2015-03-12 22:32:55 +0000

    add more tests for parseDate

--- test/spec/services/dateservice.js
+++ test/spec/services/dateservice.js
@@ -23,8 +23,16 @@ describe('Service: dateService', function () {
     it('should parse dates of format dd/mm/yyyy',function() {
         var day1 = "31/03/2000";
         var day2 = "04/02/2000";
+        var day3 = "31/12/2015";
+        var day4 = "01/01/2000";
         expect(dateService.parseDate(day1).getMonth()).toBe(2);
         expect(dateService.parseDate(day2).getDate()).toBe(4);
+        expect(dateService.parseDate(day3).getMonth()).toBe(11);
+        expect(dateService.parseDate(day4).getMonth()).toBe(0);
+        expect(dateService.parseDate(day3).getDate()).toBe(31);
+        expect(dateService.parseDate(day4).getDate()).toBe(1);
+        expect(dateService.parseDate(day4).getFullYear()).toBe(2000);
+        expect(dateService.parseDate(day3).getFullYear()).toBe(2015);
     });
 
 });
