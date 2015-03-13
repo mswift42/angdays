e4fd5b0bfe732e46822620630b29e160e265607b
martin.haesler <martin.haesler@gmail.com>
2015-03-13 15:25:14 +0000

    add test for deleteTask and editTask fn

--- test/spec/controllers/tasklist.js
+++ test/spec/controllers/tasklist.js
@@ -23,5 +23,19 @@ describe('Controller: TasklistCtrl', function () {
         expect(scope.hideContent).toBe(true);
         scope.revealContent();
         expect(scope.hideContent).toBe(false);
-  });
+    });
+    
+    it('should delete a task from scope.tasks', function() {
+     scope.tasks = [{"summary":"task1","id":"123"},
+                   {"summary": "task2", "id":"124"}];
+        scope.deleteTask(scope.tasks[0]);
+        expect(scope.tasks.length).toBe(1);
+        expect(scope.tasks[0].id).toBe("124");
+    });
+    it('should edit a task with a given id', function() {
+        scope.tasks = [{"summary":"task1", "id":"123"},
+                       {"summary":"task2", "id": "124"}];
+        scope.editTask({"summary":"taskone","id":"123"});
+        expect(scope.tasks[0].summary).toBe("taskone");
+    });
 });
